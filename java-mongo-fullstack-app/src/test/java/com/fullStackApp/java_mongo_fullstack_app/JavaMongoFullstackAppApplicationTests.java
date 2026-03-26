6ab163d553ff9e61676ba564be90f80b1884c679
ankucse <49396582+ankucse@users.noreply.github.com>
2026-03-26 15:28:46 +0530

    Initial Commit

---
+++ java-mongo-fullstack-app/src/test/java/com/fullStackApp/java_mongo_fullstack_app/JavaMongoFullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStackApp.java_mongo_fullstack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongoFullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
